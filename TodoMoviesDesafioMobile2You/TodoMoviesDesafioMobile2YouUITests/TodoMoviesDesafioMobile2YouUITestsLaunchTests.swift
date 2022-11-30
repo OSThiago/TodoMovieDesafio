54d5062e1fc32e305047d0b96f9100821e4197db
Thiago de Oliveira Sousa <thiagos2000@gmail.com>
2022-11-30 12:11:23 -0300

    feat: add inital files

---
+++ TodoMoviesDesafioMobile2You/TodoMoviesDesafioMobile2YouUITests/TodoMoviesDesafioMobile2YouUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TodoMoviesDesafioMobile2YouUITestsLaunchTests.swift
+//  TodoMoviesDesafioMobile2YouUITests
+//
+//  Created by Thiago de Oliveira Sousa on 30/11/22.
+//
+
+import XCTest
+
+final class TodoMoviesDesafioMobile2YouUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
