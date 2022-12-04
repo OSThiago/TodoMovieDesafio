050501aaf3dab5e149653ef2f45b6f48dc1503ac
Thiago de Oliveira Sousa <thiagos2000@gmail.com>
2022-12-04 11:38:42 -0300

    feat: add similar movies

--- TodoMoviesDesafioMobile2You/TodoMoviesDesafioMobile2You/Views/MovieDetails/MovieDetaislViewController.swift
+++ TodoMoviesDesafioMobile2You/TodoMoviesDesafioMobile2You/Views/MovieDetails/MovieDetaislViewController.swift
@@ -21,18 +21,53 @@ class MovieDetaislViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setup()
+        setupTableView()
     }
     
     func setup() {
         // Faz request do filme
         viewModel.fetchMovieDetail(movieID: 436270)
     }
+    
+    func setupTableView() {
+        movieDetailsView.similarMoviesTableView.delegate = self
+        movieDetailsView.similarMoviesTableView.dataSource = self
+        movieDetailsView.similarMoviesTableView.rowHeight = UIScreen.main.bounds.height * 0.09
+    }
 }
 
+// MARK: - DELEGATES
 extension MovieDetaislViewController: MovieDetaisDelegate {
     func movieDetailsFeteched() {
         DispatchQueue.main.async {
             self.movieDetailsView.setupWith(movieDetails: self.viewModel.movieDetailsModel)
+            self.movieDetailsView.similarMoviesTableView.reloadData()
         }
     }
+}
+
+// MARK: - TABLE VIEW
+
+extension MovieDetaislViewController: UITableViewDataSource, UITableViewDelegate {
+    // DATASOURCER
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: - PEGAR A QUANTIDADE VINDA DA API E RETORNAR
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: - PEGAR DADOS DA API E ADIIONAR NA CELULAR
+        
+        // Create cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SimilarMovieTableViewCell.identifier, for: indexPath) as? SimilarMovieTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        // TODO: - TROCAR PARA OS DADOS VINDO DA API
+        cell.setupWith(similarMovie: SimilarMovieModel.JusticeLeague)
+        
+        return cell
+    }
+    
+    // DELEGATE
 }
--- TodoMoviesDesafioMobile2You/TodoMoviesDesafioMobile2You/Views/MovieDetails/SimilarMovieTableViewCell.swift
+++ TodoMoviesDesafioMobile2You/TodoMoviesDesafioMobile2You/Views/MovieDetails/SimilarMovieTableViewCell.swift
@@ -9,4 +9,140 @@ import UIKit
 
 class SimilarMovieTableViewCell: UITableViewCell {
     
+    private let screen = UIScreen.main.bounds
+    
+    static let identifier = "SimilarMovieTableViewCell"
+    
+    // MARK: - COMPONENTS
+    // IMAGE
+    lazy var image = makeImage()
+    
+    // TITLE
+    lazy var title = makeTitle()
+    
+    // RELEASE DATE
+    lazy var releaseDate = makeReleaseDate()
+    
+    // GENRES
+    lazy var genres = makeGenres()
+    
+    // MARK: - INIT
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupWith(similarMovie: SimilarMovieModel) {
+        self.title.text = similarMovie.title
+        self.releaseDate.text = similarMovie.release_date
+        
+        // genres
+        // Concatena a lista de generos
+        // TODO: - Melhorar a logica e por em uma função
+        // TODO: - Criar uma lista de generos padrão para receber o id do genero
+        self.genres.text?.removeAll()
+        for genre in similarMovie.genre_ids {
+            self.genres.text?.append("\(genre), ")
+        }
+        // Para remover a virgula e o espaço final
+        self.genres.text?.removeLast(2)
+        
+        // image
+        if let url = URL(string: "https://image.tmdb.org/t/p/w500/\(similarMovie.poster_path)") {
+            self.image.load(url: url)
+        }
+    }
+    
+    // MARK: - MAKE COMPONENTES
+    
+    // IMAGE
+    func makeImage() -> UIImageView {
+        let image = UIImageView()
+        
+        // Placeholder
+        let rect = CGRect(x: 0, y: 0, width: 50, height: 50)
+        let renderer = UIGraphicsImageRenderer(bounds: rect)
+        let grayImg = renderer.image { context in
+            context.cgContext.setFillColor(gray: 1, alpha: 0.5)
+            context.cgContext.fill(rect)
+        }
+        image.image = grayImg
+        
+        // Config Images
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        return image
+    }
+    
+    // TITLE
+    func makeTitle() -> UILabel {
+        let title = UILabel()
+        title.text = "Title"
+        title.numberOfLines = 2
+        title.lineBreakMode = .byTruncatingTail
+        title.font = UIFont.boldSystemFont(ofSize: 12)
+        return title
+    }
+    
+    // RELEASE DATE
+    func makeReleaseDate() -> UILabel {
+        let date = UILabel()
+        date.text = "0000"
+        date.font = UIFont.systemFont(ofSize: 8)
+        return date
+    }
+    
+    // GENRES
+    func makeGenres() -> UILabel {
+        let genres = UILabel()
+        genres.text = ""
+        genres.font = UIFont.systemFont(ofSize: 8)
+        return genres
+    }
+}
+
+// MARK: - VIEW SETUP
+extension SimilarMovieTableViewCell: ViewSetup {
+    func addViews() {
+        addSubview(image)
+        addSubview(title)
+        addSubview(releaseDate)
+        addSubview(genres)
+    }
+    
+    func setupConstraints() {
+        // Image
+        image.snp.makeConstraints { make in
+            make.size.width.equalTo(50)
+            //make.size.height.equalTo(120)
+            make.leading.equalToSuperview()
+            make.verticalEdges.equalToSuperview().inset(5)
+        }
+        
+        // title
+        title.snp.makeConstraints { make in
+            make.leading.equalTo(image.snp.trailing).offset(10)
+            make.top.equalToSuperview().offset(20)
+        }
+        
+        // release date
+        releaseDate.snp.makeConstraints { make in
+            make.leading.equalTo(image.snp.trailing).offset(10)
+            make.top.equalTo(title.snp.bottom).offset(5)
+        }
+        
+        // genres
+        genres.snp.makeConstraints { make in
+            make.leading.equalTo(releaseDate.snp.trailing).offset(10)
+            make.top.equalTo(title.snp.bottom).offset(5)
+        }
+    }
+    
+    func otherConfigurations() {
+        
+    }
 }
